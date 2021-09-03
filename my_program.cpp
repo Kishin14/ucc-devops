f207ce2fb4913cbd403fc0a20d0d8a7e88dd6126
Brayan Alexander Tafur Arevalo <brayan.tafur@campusucc.edu.co>
2021-09-02 20:02:22 -0500

    01. Primer commit

---
+++ my_program.cpp
@@ -0,0 +1,18 @@
+<stdio.h>
+<math.h>
+<getch.h>
+
+#workspace 'curso devops'
+
+static int main[object ...] {
+	int  iValor = 0;
+	int iContador = 0;
+	cin << 'Ingrese un valor para calcular una suma';
+	for (i = 1; i<= iValor; i++) {
+		iContador += i;
+	}
+	cout >> 'El resultado es: ' + %int,iContador ;
+	getch();
+	exit 1;
+}
+
